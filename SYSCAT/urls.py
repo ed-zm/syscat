384cf249e55b6b6605e5cfcb2b95e04dffb4ef61
Eduardo Zarraga <eduardoandres8391@hotmail.com>
2015-04-06 02:02:46 -0430

    continuando con la carga de archivos estaticos y creacion de algunas plantillas

--- SYSCAT/urls.py
+++ SYSCAT/urls.py
@@ -8,6 +8,9 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', TemplateView.as_view(template_name= 'index.html'), name='home'),
     #url(r'^blog/', include('blog.urls')),
-
+    url(r'^registro/', TemplateView.as_view(template_name = 'static/templates/registro.html'), name = 'registro'),
+    url(r'^busqueda/', TemplateView.as_view(template_name = 'static/templates/busqueda.html'), name = 'busqueda'),
+    url(r'^login/', TemplateView.as_view(template_name = 'login.html'), name = 'login'),
+                       
     url(r'^admin/', include(admin.site.urls)),
 )
